bfcfdb4fa982d1c74e16557b43363ced1759e712
Allan kipruto <koechallanhi@gmail.com>
2024-02-11 09:29:30 +0300

    the console 0.0.1 updates with comments

--- console.py
+++ console.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-
+"""Console module."""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """Command interpreter class."""
+
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Quit command to exit the program."""
         return True
-
-    def help_quit(self):
-        print("Quit command to exit the program")
-        print()
 
     def do_EOF(self, arg):
-        """Exit the program gracefully"""
-        print("")
+        """EOF to exit the program."""
         return True
+
+    def emptyline(self):
+        """Empty line should not execute anything."""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
