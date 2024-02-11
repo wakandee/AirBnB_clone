3ec35ae57530c9a627774b4112c5cc31b02e6a65
Allan kipruto <koechallanhi@gmail.com>
2024-02-11 11:51:24 +0300

    airbnb console

--- console.py
+++ console.py
@@ -3,36 +3,120 @@
 This module contains the entry point of the command interpreter.
 """
 import cmd
-import sys
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """This class defines the command interpreter."""
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
-        """Quit command to exit the program."""
-        print("")
-        return True
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it, and prints the id."""
+        if not arg:
+            print("** class name missing **")
+            return
 
-    def do_EOF(self, arg):
-        """EOF signal to exit the program."""
-        print("")
-        return True
+        class_name = arg.split()[0]
+        if class_name not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        print(storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        del storage.all()[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representations of all instances."""
+        if not arg:
+            print([str(instance) for instance in storage.all().values()])
+        else:
+            class_name = arg.split()[0]
+            if class_name not in ["BaseModel"]:
+                print("** class doesn't exist **")
+                return
+
+            print([str(instance) for key, instance in storage.all().items() if key.startswith(class_name)])
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id."""
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        if args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        key = "{}.{}".format(args[0], args[1])
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        setattr(storage.all()[key], args[2], args[3])
+        storage.all()[key].save()
 
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
         pass
 
-    def precmd(self, line):
-        """Execute a command when it is entered."""
-        if line == "python3 -m unittest discover tests":
-            return line
-        return cmd.Cmd.precmd(self, line)
-
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == "-c":
-        HBNBCommand().onecmd(" ".join(sys.stdin.readlines()))
-    else:
-        HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
