5676c91dce4a51ba8466cf15853b91fdd9adb740
Allan kipruto <koechallanhi@gmail.com>
2024-02-11 08:23:10 +0300

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,61 @@
 #!/usr/bin/python3
+"""
+Defines the BaseModel class.
+"""
 
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel class for defining common attributes/methods."""
-    def __init__(self):
-        """Initialize BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    """
+    Base class for all models in this project.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new BaseModel instance.
+
+        Args:
+            *args: Variable length argument list (not used).
+            **kwargs: Arbitrary keyword arguments.
+                If provided, set attributes according to the key/value pairs
+                in kwargs.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key,
+                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """Return string representation of BaseModel."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        Returns a string representation of the BaseModel instance.
+
+        Returns:
+            str: String representation of the BaseModel instance.
+        """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update updated_at attribute with current datetime."""
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return dictionary representation of BaseModel."""
+        """
+        Returns a dictionary representation of the BaseModel instance.
+
+        Returns:
+            dict: Dictionary representation of the BaseModel instance.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,43 +1,78 @@
-#!/usr/bin/python3
-
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class TestBaseModel(unittest.TestCase):
-    """Test cases for BaseModel class."""
+    """
+    Test the BaseModel class
+    """
+
+    def test_instance_creation(self):
+        """
+        Test instance creation
+        """
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
+
     def test_attributes(self):
-        """Test BaseModel attributes."""
+        """
+        Test instance attributes
+        """
         my_model = BaseModel()
         self.assertTrue(hasattr(my_model, 'id'))
         self.assertTrue(hasattr(my_model, 'created_at'))
         self.assertTrue(hasattr(my_model, 'updated_at'))
 
+    def test_str_method(self):
+        """
+        Test __str__ method
+        """
+        my_model = BaseModel()
+        self.assertEqual(str(my_model), "[BaseModel] ({}) {}".format(
+            my_model.id, my_model.__dict__))
+
     def test_save_method(self):
-        """Test BaseModel save method."""
+        """
+        Test save method
+        """
         my_model = BaseModel()
         old_updated_at = my_model.updated_at
         my_model.save()
         self.assertNotEqual(old_updated_at, my_model.updated_at)
 
     def test_to_dict_method(self):
-        """Test BaseModel to_dict method."""
+        """
+        Test to_dict method
+        """
         my_model = BaseModel()
-        my_model.name = "My First Model"
+        my_model.name = "My_First_Model"
         my_model.my_number = 89
-        obj_dict = my_model.to_dict()
-        self.assertTrue(isinstance(obj_dict, dict))
-        self.assertEqual(obj_dict['__class__'], 'BaseModel')
-        self.assertEqual(obj_dict['name'], 'My First Model')
-        self.assertEqual(obj_dict['my_number'], 89)
+        my_model_json = my_model.to_dict()
+        self.assertEqual(my_model_json['id'], my_model.id)
+        self.assertEqual(my_model_json['__class__'], 'BaseModel')
+        self.assertEqual(my_model_json['name'], "My_First_Model")
+        self.assertEqual(my_model_json['my_number'], 89)
+        self.assertEqual(my_model_json['created_at'], my_model.created_at.strftime(
+            "%Y-%m-%dT%H:%M:%S.%f"))
+        self.assertEqual(my_model_json['updated_at'], my_model.updated_at.strftime(
+            "%Y-%m-%dT%H:%M:%S.%f"))
 
-    def test_str_method(self):
-        """Test BaseModel __str__ method."""
-        my_model = BaseModel()
-        my_model.name = "My First Model"
-        my_model.my_number = 89
-        expected_str = "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__)
-        self.assertEqual(str(my_model), expected_str)
+    def test_kwargs_datetime(self):
+        """
+        Test if datetime strings are properly converted to datetime objects
+        """
+        kwargs = {
+            'id': '123',
+            'created_at': '2024-02-11T12:00:00.000000',
+            'updated_at': '2024-02-11T12:00:00.000000'
+        }
+        my_model = BaseModel(**kwargs)
+        self.assertEqual(my_model.id, '123')
+        self.assertEqual(my_model.created_at,
+                         datetime(2024, 2, 11, 12, 0, 0))
+        self.assertEqual(my_model.updated_at,
+                         datetime(2024, 2, 11, 12, 0, 0))
 
 
 if __name__ == '__main__':
