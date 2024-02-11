0e03fef467c47379b7834915607b2d0b127973e5
Allan kipruto <koechallanhi@gmail.com>
2024-02-11 12:41:07 +0300

    user.py

--- models/user.py
+++ models/user.py
@@ -2,14 +2,13 @@
 """Module for User class."""
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    """Class representing a user."""
+    """ User class that inherits from BaseModel """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """Initialize User instance."""
+        """ Initialize User """
         super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
